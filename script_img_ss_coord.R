a3c2a1b6a147a1d1146fd537a2ac50ed06a39ac6
Clara Reichert <c.reichert@meropy.com>
2020-06-24 15:56:04 +0200

    sur diff couches et appliqué au nvx jeu de données (03/06)

--- script_img_ss_coord.R
+++ script_img_ss_coord.R
@@ -1,20 +1,23 @@
-#d:1 -> M2:159 -> pictdata:1758
 #git push -u origin master
+#attention, bug quand il y a pas du tout de coordonnées autour, il faut faire commencer au bon endroit
 
 #pas que les nvlles coordonnées dans le csv
 
-D=M2[159:176,]    #limite le jeu de données à la partie ou il y a des manques (manuel)
+D=M2
 D$d_h=as.POSIXct(D$nom_images , format = "chassis_%Y-%m-%d_%H.%M.%S.jpg")
 l=length (D$Latitude)
-
-for (i in 1:l){     #parcours D
-  ip=1758
+L=character(length=0)
+c=1
+for (i in 23:l){     #parcours D
+  ip=1
   if (is.na(D[i,1]==T)){    #s'il y a pas de latitude
     ##trouver les indices de pictdata tel que l'heure soit entourée
+  L[c]=i
+  c=c+1
   while(as.numeric(D[i,4])<as.numeric(pictdata[ip,3]) || as.numeric(D[i,4])>as.numeric(pictdata[ip+1,3])) {
     ip=ip+1
   } 
-    
+  print("YES")
   t1=as.numeric(pictdata[ip,3])
   t2=as.numeric(pictdata[ip+1,3])
   lat1=as.numeric(pictdata[ip,1])
@@ -31,5 +34,5 @@ for (i in 1:l){     #parcours D
   D[i,2]=coeflon*as.numeric(D[i,4])+Blon
   }
 }
-
-write.csv2(M2, file = "img_avec_coord_calculees.csv", row.names = FALSE)
+D=D[L,]
+write.csv2(D, file = "img_avec_coord_calculees.csv", row.names = FALSE)
