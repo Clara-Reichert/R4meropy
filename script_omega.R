373ed02aa0caf12c84b5b3dc568ace59f30a6944
Clara Reichert <c.reichert@meropy.com>
2020-06-30 16:06:32 +0200

    ajusté nom colonnes

--- script_omega.R
+++ script_omega.R
@@ -1,5 +1,4 @@
 ####################### extraire longitude, latitude et date_heure (pour les données GNSS issues du GPS omega)
-
 GNSS1<- read.csv2("C:/Users/clare/R4meropy/gnss_2020-06-03-14-08.txt", sep=",",header=F)
 GNSS1=GNSS1[-1,]
 GNSS2<- read.csv2("C:/Users/clare/R4meropy/gnss_2020-06-03-14-42.txt", sep=",",header=F)
@@ -7,9 +6,10 @@ GNSS2=GNSS2[-1,]
 GNSS=rbind(GNSS1,GNSS2)
 GNSS$Latitude=substr(GNSS$V2,11,20)                                   
 GNSS$Longitude=substr(GNSS$V3,13,20)
-GNSS$d_h=substr(GNSS$V6,14,33)
-GNSS=subset(GNSS,select=c("Latitude","Longitude","d_h"))
-GNSS$d_h=as.POSIXct(GNSS$d_h, format = "%Y-%m-%d-%H-%M-%S")
-GNSS$d_h=GNSS$d_h+2*3600
+GNSS$DateTime=substr(GNSS$V6,14,33)
+GNSS=subset(GNSS,select=c("Latitude","Longitude","DateTime"))
+GNSS$DateTime=as.POSIXct(GNSS$DateTime, format = "%Y-%m-%d-%H-%M-%S")
+GNSS$DateTime=GNSS$DateTime+2*3600  
 pictdata=GNSS
+
 #########################
