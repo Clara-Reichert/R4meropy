80fc238719082ac8d8f7d41c48d2d6f2d7fa13c0
Clara Reichert <c.reichert@meropy.com>
2020-06-30 15:18:38 +0200

    2h de décalage + pictdata

--- script_omega.R
+++ script_omega.R
@@ -1,3 +1,5 @@
+####################### extraire longitude, latitude et date_heure (pour les données GNSS issues du GPS omega)
+
 GNSS1<- read.csv2("C:/Users/clare/R4meropy/gnss_2020-06-03-14-08.txt", sep=",",header=F)
 GNSS1=GNSS1[-1,]
 GNSS2<- read.csv2("C:/Users/clare/R4meropy/gnss_2020-06-03-14-42.txt", sep=",",header=F)
@@ -8,4 +10,6 @@ GNSS$Longitude=substr(GNSS$V3,13,20)
 GNSS$d_h=substr(GNSS$V6,14,33)
 GNSS=subset(GNSS,select=c("Latitude","Longitude","d_h"))
 GNSS$d_h=as.POSIXct(GNSS$d_h, format = "%Y-%m-%d-%H-%M-%S")
-GNSS$d_h=GNSS$d_h+3600           
+GNSS$d_h=GNSS$d_h+2*3600
+pictdata=GNSS
+#########################
