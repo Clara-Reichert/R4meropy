ae2ff62e637ed14faed2f6de5502472697a04e6f
Clara Reichert <c.reichert@meropy.com>
2020-07-01 12:18:42 +0200

    eviter doublons de noms

--- script_img_ss_coord.R
+++ script_img_ss_coord.R
@@ -6,13 +6,13 @@
 D=M2
 D$d_h=as.POSIXct(D$nom_images , format = "chassis_%Y-%m-%d_%H.%M.%S.jpg")
 l=length (D$Latitude)
-L=character(length=0)
+L1=character(length=0)
 c=1
-for (i in 23:l){     #parcours D
+for (i in 1:l){     #parcours D
   ip=1
   if (is.na(D[i,1]==T)){    #s'il y a pas de latitude
     ##trouver les indices de pictdata tel que l'heure soit entourée
-  L[c]=i
+  L1[c]=i
   c=c+1
   while(as.numeric(D[i,4])<as.numeric(pictdata[ip,3]) || as.numeric(D[i,4])>as.numeric(pictdata[ip+1,3])) {
     ip=ip+1
@@ -34,5 +34,5 @@ for (i in 23:l){     #parcours D
   D[i,2]=coeflon*as.numeric(D[i,4])+Blon
   }
 }
-D=D[L,]
+D=D[L1,]
 write.csv2(D, file = "img_avec_coord_calculees.csv", row.names = FALSE)
