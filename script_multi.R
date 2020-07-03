0f17e48b19d31b8a9acafb5dc7de5db17a535ac6
Clara Reichert <c.reichert@meropy.com>
2020-07-03 15:49:49 +0200

    commentaires

--- script_multi.R
+++ script_multi.R
@@ -1,5 +1,9 @@
-####################### extraire longitude, latitude et date_heure (pour les données GNSS u=issues du capteur multispectral)
+###############################################################
+### extraire longitude, latitude et date_heure              ###
+### (pour les données GNSS issues du capteur multispectral) ###
+###############################################################
 
+## lecture du jeu de données ###
 #importer le jeu de donnees
 pictdata <- read.delim("C:\\Users\\clare\\Desktop\\R_GNSS\\multi\\pictdata_29042000.txt")
 
@@ -19,43 +23,49 @@ pictdata$Longitude=pictdata$AEX
 #choisir les bonnes colonnes
 pictdata=subset(pictdata,select=c(8,9,25))
 
-#enlever les lignes pour lesquelles il n'y a pas de coordonnees (utile?)
+#enlever les lignes pour lesquelles il n'y a pas de coordonnees 
 pictdata=subset(pictdata,Latitude!="N99:99.9999")
 
-#Convertir les coordonees du format N45:12.1234 ? 45.121234 
-#pas encore pris en compte N/S ?
+## Convertir les coordonees de degrés-minute-fraction de minute à degrés décimaux ##
 #pour la Latitude
 ########
-L=character(length=0)
-for ( i in 1:length(pictdata$Latitude)){
-  A=as.character(pictdata[i,1])
-  B1=as.numeric(substr(A,2,3))
-  B2=substr(A,5,6)
-  B3=substr(A,8,11)
+L=character(length=0)   #liste qui va contenir les nouelles coordonnées
+for ( i in 1:length(pictdata$Latitude)){    #parcours le jeu de données
+  A=as.character(pictdata[i,1])   #latitude DMFM
+  B1=as.numeric(substr(A,2,3))    #degrés
+  B2=substr(A,5,6)                #minutes
+  B3=substr(A,8,11)               #fractions de minutes
+  #combinaison de minutes et fractions de minutes
   B2=as.numeric(paste(B2,B3,sep="."))
+  # conversion de tout en degrés
   B=B1+B2/60
+  # prendre en considération N/S => +/-
   if(substr(A,1,1)=="S"){
     B=-B
   }
-  
   L[i]=as.double(round(B,6))
 }
+#remplacer par les nouvelles latitudes dans le jeu de données
 pictdata$Latitude=L
 #######
 #pour la longitude
-L=character(length=0)
-for ( i in 1:length(pictdata$Longitude)){
-  A=as.character(pictdata[i,2])
-  B1=as.numeric(substr(A,2,4))
-  B2=substr(A,6,7)
-  B3=substr(A,9,12)
+L=character(length=0)   #liste qui va contenir les nouelles coordonnées
+for ( i in 1:length(pictdata$Longitude)){    #parcours le jeu de données
+  A=as.character(pictdata[i,2])   #longitude DMFM
+  B1=as.numeric(substr(A,2,4))    #degrés
+  B2=substr(A,6,7)                #minutes
+  B3=substr(A,9,12)               #fractions de minutes
+  #combinaison de minutes et fractions de minutes
   B2=as.numeric(paste(B2,B3,sep="."))
+  # conversion de tout en degrés
   B=B1+B2/60
+  # prendre en considération E/O => +/-
   if(substr(A,1,1)=="O"){
     B=-B
   }
   
   L[i]=as.double(round(B,6))
 }
+#remplacer par les nouvelles longitudes dans le jeu de données
 pictdata$Longitude=L
 #########################
